97eab63cf6facca026bb1300d17f5c99f0f194d1
Flavio Assis <flavioh.dev@gmail.com>
2023-11-02 14:14:15 -0300

    Initial commit

---
+++ api/src/main/java/com/flavio/easystock/EasyStockApplication.java
@@ -0,0 +1,13 @@
+package com.flavio.easystock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyStockApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/flavio/dslist/EasyStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flavio.easystock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
