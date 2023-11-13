7f20d7d8da330fff9ecf46af826bb391e0895c00
Flavio Assis <flavioh.dev@gmail.com>
2023-11-13 18:33:40 -0300

    Refactor product type controller

--- api/src/main/java/com/flavio/easystock/controllers/ProductTypeController.java
+++ api/src/main/java/com/flavio/easystock/controllers/ProductTypeController.java
@@ -1,21 +1,67 @@
 package com.flavio.easystock.controllers;
 
-import com.flavio.easystock.entities.ProductType;
+import com.flavio.easystock.dtos.responses.ProductTypeResponse;
+import com.flavio.easystock.requests.ProductTypeRequest;
 import com.flavio.easystock.services.ProductTypeService;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("product-types")
+@RequestMapping("/v1/product-types")
 public class ProductTypeController {
 
-    @Autowired
-    private ProductTypeService productTypeService;
+    private final ProductTypeService productTypeService;
+    private final ModelMapper mapper;
+
+    public ProductTypeController(ProductTypeService productTypeService, ModelMapper mapper) {
+        this.productTypeService = productTypeService;
+        this.mapper = mapper;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductTypeResponse> create(@Valid @RequestBody ProductTypeRequest request) {
+        var result = productTypeService.create(request);
+        var mappedDto = mapper.map(result, ProductTypeResponse.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(mappedDto);
+    }
 
     @GetMapping
-    public List<ProductType> findAll() {
-        return productTypeService.findAll();
+    public ResponseEntity<List<ProductTypeResponse>> findAll() {
+        var result = productTypeService.findAll();
+        var mappedDto = result
+                .stream()
+                .map(x -> mapper.map(x, ProductTypeResponse.class))
+                .toList();
+
+        return ResponseEntity.status(HttpStatus.OK).body(mappedDto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductTypeResponse> findById(@PathVariable Long id) {
+        var result = productTypeService.findById(id);
+        var mappedDto = mapper.map(result, ProductTypeResponse.class);
+
+        return ResponseEntity.status(HttpStatus.OK).body(mappedDto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductTypeResponse> update(@PathVariable Long id, @Valid @RequestBody ProductTypeRequest request) {
+        var result = productTypeService.update(id, request);
+        var mappedDto = mapper.map(result, ProductTypeResponse.class);
+
+        return ResponseEntity.status(HttpStatus.OK).body(mappedDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        productTypeService.delete(id);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
