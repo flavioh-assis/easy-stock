85fd39ac440a3bbfa4836e2e0c398f3805363b05
Flavio Assis <flavioh.dev@gmail.com>
2023-11-13 18:29:49 -0300

    Create product type request

---
+++ api/src/main/java/com/flavio/easystock/requests/ProductTypeRequest.java
@@ -0,0 +1,17 @@
+package com.flavio.easystock.requests;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class ProductTypeRequest {
+
+    @NotBlank(message = "Name is required")
+    @Size(max = 255, message = "Name must have a maximum of 255 characters.")
+    private String name;
+}
