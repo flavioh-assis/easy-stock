8203e40fe3472af8dc11e4bfe868097a32e34ab1
Flavio Assis <flavioh.dev@gmail.com>
2023-11-13 18:30:30 -0300

    Refactor product type service

--- api/src/main/java/com/flavio/easystock/services/ProductTypeService.java
+++ api/src/main/java/com/flavio/easystock/services/ProductTypeService.java
@@ -1,8 +1,10 @@
 package com.flavio.easystock.services;
 
 import com.flavio.easystock.entities.ProductType;
+import com.flavio.easystock.exceptions.EntityNotFoundException;
 import com.flavio.easystock.repositories.ProductTypeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.flavio.easystock.requests.ProductTypeRequest;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,11 +13,47 @@ import java.util.List;
 @Service
 public class ProductTypeService {
 
-    @Autowired
-    private ProductTypeRepository productTypeRepository;
+    private final ProductTypeRepository productTypeRepository;
+    private final ModelMapper mapper;
+
+    public ProductTypeService(ProductTypeRepository productTypeRepository, ModelMapper mapper) {
+        this.productTypeRepository = productTypeRepository;
+        this.mapper = mapper;
+    }
+
+    @Transactional
+    public ProductType create(ProductTypeRequest request) {
+        var entity = mapper.map(request, ProductType.class);
+
+        return productTypeRepository.save(entity);
+    }
 
     @Transactional(readOnly = true)
     public List<ProductType> findAll() {
         return productTypeRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public ProductType findById(Long id) {
+        return productTypeRepository
+                .findById(id)
+                .orElseThrow(
+                        () -> new EntityNotFoundException("product type", id)
+                );
+    }
+
+    @Transactional
+    public ProductType update(Long id, ProductTypeRequest request) {
+        var entity = this.findById(id);
+        mapper.map(request, entity);
+
+        return productTypeRepository.save(entity);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        var entity = this.findById(id);
+
+        productTypeRepository.delete(entity);
     }
 }
