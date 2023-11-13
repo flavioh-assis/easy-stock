bb871c32fd70bb7cf682070a40e838b6c1525dee
Flavio Assis <flavioh.dev@gmail.com>
2023-11-13 18:28:13 -0300

    Refactor product type response

---
+++ api/src/main/java/com/flavio/easystock/dtos/responses/ProductTypeResponse.java
@@ -0,0 +1,16 @@
+package com.flavio.easystock.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ProductTypeResponse {
+
+    private Long id;
+    private String name;
+}
