e78b5ca5e9a676993ac427b9eeaa9de43d94cc49
Flavio Assis <flavioh.dev@gmail.com>
2023-11-13 22:27:17 -0300

    Refactor product entity

--- api/src/main/java/com/flavio/easystock/entities/Product.java
+++ api/src/main/java/com/flavio/easystock/entities/Product.java
@@ -1,12 +1,22 @@
 package com.flavio.easystock.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.flavio.easystock.dtos.requests.ProductRequest;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     private Long id;
 
     @Column(nullable = false)
@@ -15,45 +25,14 @@ public class Product {
     @Column(nullable = false)
     private String description;
 
-    @ManyToOne
+    @JsonIgnoreProperties("products")
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
     @JoinColumn(name = "type_id", nullable = false)
     private ProductType type;
 
-    protected Product() {
-    }
-
-    public Product(Long id, String name, String description, ProductType type) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.type = type;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public ProductType getType() {
-        return type;
-    }
-
-    public void setType(ProductType type) {
+    public Product(ProductRequest request, ProductType type) {
+        this.name = request.getName();
+        this.description = request.getDescription();
         this.type = type;
     }
 }
