57689cffaa16afecb1853f004e9c59e610488376
Flavio Assis <flavioh.dev@gmail.com>
2023-11-14 23:33:43 -0300

    Create add and remove endpoints for stock controller

--- api/src/main/java/com/flavio/easystock/controllers/StockController.java
+++ api/src/main/java/com/flavio/easystock/controllers/StockController.java
@@ -1,27 +1,60 @@
 package com.flavio.easystock.controllers;
 
-import com.flavio.easystock.entities.Stock;
+import com.flavio.easystock.dtos.requests.StockRequest;
+import com.flavio.easystock.dtos.responses.StockResponse;
 import com.flavio.easystock.services.StockService;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
-@RequestMapping("/stocks")
+@RequestMapping("/v1/stocks")
 public class StockController {
 
-    @Autowired
-    private StockService stockService;
+    private final StockService stockService;
+    private final ModelMapper mapper;
+
+    public StockController(StockService stockService, ModelMapper mapper) {
+        this.stockService = stockService;
+        this.mapper = mapper;
+    }
 
     @GetMapping
-    public List<Stock> findAll() {
-        return stockService.findAll();
+    public ResponseEntity<List<StockResponse>> findAll() {
+        var result = stockService.findAll();
+        var mappedDto = result
+                .stream()
+                .map(x -> mapper.map(x, StockResponse.class))
+                .toList();
+
+        return ResponseEntity.status(HttpStatus.OK).body(mappedDto);
     }
 
     @GetMapping("/{id}")
-    public Optional<Stock> findById(@PathVariable Long id) {
-        return stockService.findById(id);
+    public ResponseEntity<StockResponse> findById(@PathVariable Long id) {
+        var result = stockService.findById(id);
+        var mappedDto = mapper.map(result, StockResponse.class);
+
+        return ResponseEntity.status(HttpStatus.OK).body(mappedDto);
+    }
+
+    @PutMapping("/{id}/add")
+    public ResponseEntity<StockResponse> add(@PathVariable Long id, @Valid @RequestBody StockRequest request) {
+        var result = stockService.add(id, request);
+        var mappedDto = mapper.map(result, StockResponse.class);
+
+        return ResponseEntity.status(HttpStatus.OK).body(mappedDto);
+    }
+
+    @PutMapping("/{id}/remove")
+    public ResponseEntity<StockResponse> remove(@PathVariable Long id, @Valid @RequestBody StockRequest request) throws Exception {
+        var result = stockService.remove(id, request);
+        var mappedDto = mapper.map(result, StockResponse.class);
+
+        return ResponseEntity.status(HttpStatus.OK).body(mappedDto);
     }
 }
