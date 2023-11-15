21a8086cbd650c48bbe786d61c228b2afb0aa378
Flavio Assis <flavioh.dev@gmail.com>
2023-11-14 23:33:25 -0300

    Create add and remove methods for stock service

--- api/src/main/java/com/flavio/easystock/services/StockService.java
+++ api/src/main/java/com/flavio/easystock/services/StockService.java
@@ -1,19 +1,24 @@
 package com.flavio.easystock.services;
 
+import com.flavio.easystock.dtos.requests.StockRequest;
 import com.flavio.easystock.entities.Stock;
+import com.flavio.easystock.exceptions.EntityNotFoundException;
 import com.flavio.easystock.repositories.StockRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class StockService {
 
-    @Autowired
-    private StockRepository stockRepository;
+    private final StockRepository stockRepository;
+
+    public StockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
 
     @Transactional(readOnly = true)
     public List<Stock> findAll() {
@@ -21,7 +26,76 @@ public class StockService {
     }
 
     @Transactional(readOnly = true)
-    public Optional<Stock> findById(Long id) {
-        return stockRepository.findById(id);
+    public Stock findById(Long id) {
+        return stockRepository
+                .findById(id)
+                .orElseThrow(
+                        () -> new EntityNotFoundException("stock", id)
+                );
+    }
+
+    @Transactional
+    public Stock add(Long id, StockRequest request) {
+        var entity = this.findById(id);
+
+        var currentTotal = calculateTotal(entity.getUnitPrice(), entity.getQuantity());
+        var requestTotal = calculateTotal(request.getUnitPrice(), request.getQuantity());
+
+        var newTotal = sum(currentTotal, requestTotal);
+        var newQuantity = sum(entity.getQuantity(), request.getQuantity());
+        var newUnitPrice = divide(newTotal, newQuantity);
+
+        entity.setQuantity(newQuantity);
+        entity.setUnitPrice(newUnitPrice);
+
+        return stockRepository.save(entity);
+    }
+
+    @Transactional
+    public Stock remove(Long id, StockRequest request) throws Exception {
+        var entity = this.findById(id);
+        var currentQuantity = entity.getQuantity();
+        var requestQuantity = request.getQuantity();
+
+        if (requestQuantity > currentQuantity)
+            throw new Exception("The quantity to be removed cannot be bigger than the current quantity.");
+
+        var currentTotal = calculateTotal(entity.getUnitPrice(), currentQuantity);
+        var requestTotal = calculateTotal(request.getUnitPrice(), requestQuantity);
+
+        var newTotal = subtract(currentTotal, requestTotal);
+        var newQuantity = subtract(currentQuantity, requestQuantity);
+        var newUnitPrice = divide(newTotal, newQuantity);
+
+        entity.setQuantity(newQuantity);
+        entity.setUnitPrice(newUnitPrice);
+
+        return stockRepository.save(entity);
+    }
+
+    private BigDecimal calculateTotal(BigDecimal decimal, int quantity) {
+        return decimal.multiply(new BigDecimal(quantity));
+    }
+
+    private BigDecimal divide(BigDecimal decimal, int quantity) {
+        var decimalPlaces = 2;
+
+        return decimal.divide(new BigDecimal(quantity), decimalPlaces, RoundingMode.CEILING);
+    }
+
+    private BigDecimal sum(BigDecimal value1, BigDecimal value2) {
+        return value1.add(value2);
+    }
+
+    private int sum(int value1, int value2) {
+        return value1 + value2;
+    }
+
+    private BigDecimal subtract(BigDecimal value1, BigDecimal value2) {
+        return value1.subtract(value2);
+    }
+
+    private int subtract(int value1, int value2) {
+        return value1 - value2;
     }
 }
